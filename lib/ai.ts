744ecc9816144be988e83d8a780d9d21067d7e7e
Konrad Guzek <konrad@guzek.uk>
2026-03-28 12:00:28 +0100

    feat(ai): add lib

---
+++ lib/ai.ts
@@ -0,0 +1,7 @@
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+export const gemini = google('gemini-2.0-flash-exp');
