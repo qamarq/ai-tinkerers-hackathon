52c642b4a0795e648fbd9627f9cf36bf4b431337
Konrad Guzek <konrad@guzek.uk>
2026-03-28 12:03:27 +0100

    feat(api): setup trpc

---
+++ app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
+import { appRouter } from "@/lib/trpc/routers/_app";
+import { createTRPCContext } from "@/lib/trpc/server";
+
+const handler = (req: NextRequest) =>
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: createTRPCContext,
+  });
+
+export { handler as GET, handler as POST };
