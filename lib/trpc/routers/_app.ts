33c010a756832dc2e451197e7b3c323301a9ca8a
Konrad Guzek <52281528+kguzek@users.noreply.github.com>
2026-03-28 12:07:16 +0100

    feat(api): setup trpc (#2)

---
+++ lib/trpc/routers/_app.ts
@@ -0,0 +1,12 @@
+import { createTRPCRouter, publicProcedure } from "../server";
+import { z } from "zod";
+
+export const appRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(({ input }) => {
+      return `Hello ${input.name}!`;
+    }),
+});
+
+export type AppRouter = typeof appRouter;
